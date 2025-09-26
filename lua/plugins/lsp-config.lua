d6e538a9e249731ebf2185250501bf2ccc555750
Mark <marking.dav@gmail.com>
2025-09-26 10:06:15 +0800

    Updated dependencies

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,82 +14,163 @@ return {
           "tailwindcss",
           "lua_ls",
           "ts_ls",
-          "volar",
+          -- "volar",
           "yamlls",
         },
       })
     end,
   },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  --     local lspconfig = require("lspconfig")
+  --     lspconfig.cssls.setup({
+  --       capabilities = capabilities,
+  --     })
+  --     lspconfig.tailwindcss.setup({
+  --       capabilities = capabilities,
+  --     })
+  --     lspconfig.lua_ls.setup({
+  --       capabilities = capabilities,
+  --     })
+  --     lspconfig.ts_ls.setup({
+  --       capabilities = capabilities,
+  --     })
+  --     lspconfig.volar.setup({
+  --       capabilities = capabilities,
+  --
+  --       --do `npm i -g @vue/language-server typescript`--
+  --
+  --       filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  --       init_options = {
+  --         vue = {
+  --           hybridMode = false,
+  --         },
+  --         typescript = {
+  --           tsdk = "/home/mark/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/",
+  --         },
+  --       },
+  --       on_new_config = function(new_config, new_root_dir)
+  --         local lib_path = vim.fs.find('node_modules/typescript/lib', { path = new_root_dir, upward = true })[1]
+  --         if lib_path then
+  --           new_config.init_options.typescript.tsdk = lib_path
+  --         end
+  --       end,
+  --       settings = {
+  --         typescript = {
+  --           inlayHints = {
+  --             includeInlayParameterNameHints = "all",
+  --             includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --             includeInlayFunctionParameterTypeHints = true,
+  --             includeInlayVariableTypeHints = true,
+  --             includeInlayPropertyDeclarationTypeHints = true,
+  --             includeInlayFunctionLikeReturnTypeHints = true,
+  --             includeInlayEnumMemberValueHints = true,
+  --           },
+  --         },
+  --       },
+  --     })
+  --     lspconfig.yamlls.setup({
+  --       capabilities = capabilities,
+  --     })
+  --
+  --     vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show buffer hover" })
+  --     vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+  --     vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to references" })
+  --     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
+  --     vim.keymap.set(
+  --       "n",
+  --       "<leader>D",
+  --       "<cmd>Telescope diagnostics bufnr=0<CR>",
+  --       { desc = "Show buffer diagnosticss" }
+  --     )
+  --     local signs = { Error = " ", Warn = " ", Hint = " ", info = " " }
+  --     for type, icon in pairs(signs) do
+  --       local hl = "DiagnosticSign" .. type
+  --       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  --     end
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.volar.setup({
-        capabilities = capabilities,
 
-        --do `npm i -g @vue/language-server typescript`--
-
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-        init_options = {
-          vue = {
-            hybridMode = false,
-          },
-          typescript = {
-            tsdk = "/home/mark/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/",
-          },
-        },
-        on_new_config = function(new_config, new_root_dir)
-          local lib_path = vim.fs.find('node_modules/typescript/lib', { path = new_root_dir, upward = true })[1]
-          if lib_path then
-            new_config.init_options.typescript.tsdk = lib_path
-          end
-        end,
-        settings = {
-          typescript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-        },
-      })
-      lspconfig.yamlls.setup({
+      -- define configs
+      vim.lsp.config["cssls"] = {
         capabilities = capabilities,
-      })
+      }
 
+      vim.lsp.config["tailwindcss"] = {
+        capabilities = capabilities,
+      }
+
+      vim.lsp.config["lua_ls"] = {
+        capabilities = capabilities,
+      }
+
+      vim.lsp.config["ts_ls"] = {
+        capabilities = capabilities,
+      }
+
+      -- vim.lsp.config["volar"] = {
+      --   capabilities = capabilities,
+      --   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      --   init_options = {
+      --     vue = {
+      --       hybridMode = false,
+      --     },
+      --     typescript = {
+      --       tsdk = "/home/mark/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib/",
+      --     },
+      --   },
+      --   on_new_config = function(new_config, new_root_dir)
+      --     local lib_path = vim.fs.find("node_modules/typescript/lib", { path = new_root_dir, upward = true })[1]
+      --     if lib_path then
+      --       new_config.init_options.typescript.tsdk = lib_path
+      --     end
+      --   end,
+      --   settings = {
+      --     typescript = {
+      --       inlayHints = {
+      --         includeInlayParameterNameHints = "all",
+      --         includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      --         includeInlayFunctionParameterTypeHints = true,
+      --         includeInlayVariableTypeHints = true,
+      --         includeInlayPropertyDeclarationTypeHints = true,
+      --         includeInlayFunctionLikeReturnTypeHints = true,
+      --         includeInlayEnumMemberValueHints = true,
+      --       },
+      --     },
+      --   },
+      -- }
+
+      vim.lsp.config["yamlls"] = {
+        capabilities = capabilities,
+      }
+
+      -- enable all servers
+      vim.lsp.enable("cssls")
+      vim.lsp.enable("tailwindcss")
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      -- vim.lsp.enable("volar")
+      vim.lsp.enable("yamlls")
+
+      -- keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show buffer hover" })
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to references" })
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
-      vim.keymap.set(
-        "n",
-        "<leader>D",
-        "<cmd>Telescope diagnostics bufnr=0<CR>",
-        { desc = "Show buffer diagnosticss" }
-      )
-      local signs = { Error = " ", Warn = " ", Hint = " ", info = " " }
+      vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics" })
+
+      -- diagnostic signs
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
       end
     end,
-  },
+  }
 }
